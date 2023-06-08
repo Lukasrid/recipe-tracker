1109e47acd07ca55d729c0ec7f132f0a518067b4
Lukasrid <lukas.ridstrom@gmail.com>
2023-06-08 10:20:31 +0000

    Organization and furthur linking of templates and urls

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('recipes/', views.recipes, name="recipes"),
+    path('recipe/<str:pk>/', views.recipe, name="recipe"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
+recipes = [
+    {'id': 1, 'name': 'Savoury'},
+    {'id': 2, 'name': 'Sweet'},
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'recipes': recipes}
+    return render(request, 'base/home.html', {'recipes': recipes})
 
 
-def recipes(request):
-    return render(request, 'recipes.html')
+def recipe(request, pk):
+    recipe = None
+    for i in recipes:
+        if i['id'] == int(pk):
+            recipe = i
+    context = {'recipe': recipe}
+    return render(request, 'base/recipe.html', context)
