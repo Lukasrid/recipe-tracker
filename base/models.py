2137f35e4d4b542e5bb757a21d9bb59c551645ef
Lukasrid <lukas.ridstrom@gmail.com>
2023-06-08 12:44:10 +0000

    First makemigrations made from models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    dish = models.CharField(max_length=200)
+    cuisine = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    ingredients = models.TextField()
+    method = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
