c751551d8d79b2af9e8626041bc1b7fe0505bc03
Lukasrid <lukas.ridstrom@gmail.com>
2023-06-13 07:58:54 +0000

    Add logout function

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
 
     path('', views.home, name="home"),
     path('recipe/<str:pk>/', views.recipe, name="recipe"),
