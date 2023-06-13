4d4dd27f424ed995abf8a5846d64de3fe4c4f1bf
Lukasrid <lukas.ridstrom@gmail.com>
2023-06-13 09:02:42 +0000

    Added a registration page for new users and linked it up

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
 
     path('', views.home, name="home"),
     path('recipe/<str:pk>/', views.recipe, name="recipe"),
