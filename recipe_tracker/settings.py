b9787a78046d49e756b7262c4ddb6a08bff85e33
Lukasrid <lukas.ridstrom@gmail.com>
2023-06-27 09:20:08 +0000

    allowed hosts

--- recipe_tracker/settings.py
+++ recipe_tracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-lukasrid-recipetracker-jnr65w0lvgi.ws-eu100.gitpod.io', 'flavoured-adventures.herokuapp.com' 'localhost']
+ALLOWED_HOSTS = ['8000-lukasrid-recipetracker-jnr65w0lvgi.ws-eu100.gitpod.io', 'flavoured-adventures.herokuapp.com' 'localhost', 'flavoured-adventures-f1491c2fd119.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-lukasrid-recipetracker-jnr65w0lvgi.ws-eu100.gitpod.io','https://*.127.0.0.1']
 
